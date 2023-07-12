7bc39f6847531c0b32b3757595ea52fc56076415
rc2pc2 <petriccarcc@gmail.com>
2023-07-12 12:42:48 +0200

    small refactor on index.php

--- index.php
+++ index.php
@@ -48,7 +48,9 @@
             </div>
         </div>
 
-        <?php if (!empty($_SESSION['userID']) && $_SESSION['userID'] > 0) {  ?>
+        <?php
+        echo $_SESSION['userID'];
+        if (!empty($_SESSION['userID']) && $_SESSION['userID'] > 0) {  ?>
             <div class="row">
                 <div class="col-12">
                     <h2>
